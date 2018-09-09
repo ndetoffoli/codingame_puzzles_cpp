dc10f4d7b00ee5c6c900a3f7e05131a1d9193ef1
Nicolas De Toffoli <ndetoffoli@gmail.com>
2018-09-09 21:20:56 +0200

    New class Coordinates

--- defibrillators.cpp
+++ defibrillators.cpp
@@ -1,3 +1,6 @@
+// Nicolas De Toffoli (https://github.com/ndetoffoli)
+// https://www.codingame.com/ide/puzzle/defibrillators
+
 #include <algorithm>
 #include <iostream>
 #include <sstream>
@@ -9,64 +12,91 @@
 #include <unordered_map>
 #include <limits>
 
-std::vector<std::string> splitString(const std::string & inputString,
-	char delimiter = ';')
+std::vector<std::string> splitString(const std::string & inputString, char delimiter = ';')
 {
-    std::vector<std::string> strings;
-    std::istringstream stream(inputString);
-    std::string s;
-    while (std::getline(stream, s, delimiter))
-    {
-        strings.push_back(s);
-    }
-    return strings;
+	std::vector<std::string> strings;
+	std::istringstream stream(inputString);
+	std::string s;
+	while (std::getline(stream, s, delimiter))
+	{
+		strings.push_back(s);
+	}
+	return strings;
 }
 
 double parseDouble(std::string inputValue)
 {
-    std::replace(inputValue.begin(), inputValue.end(), ',', '.');
-    return std::stod(inputValue);
+	std::replace(inputValue.begin(), inputValue.end(), ',', '.');
+	return std::stod(inputValue);
 }
+
+class Coordinates
+{
+public:
+	Coordinates():
+		lon_(0.0), lat_(0.0) {}
+
+	Coordinates(double lon, double lat):
+		lon_(lon), lat_(lat) {}
+
+	Coordinates(const std::string & lonString, const std::string & latString)
+	{
+		lon_ = parseDouble(lonString);
+		lat_ = parseDouble(latString);
+	}
+
+	std::string toString() const
+	{
+		return std::to_string(lon_) + ";"
+		+ std::to_string(lat_);
+	}
+
+	void debug() const
+	{
+		std::cerr << toString() << std::endl;
+	}
+
+public:
+	double lon_, lat_;
+};
 
 class Defibrilator
 {
-    public:
-        Defibrilator():
-            number_(0), name_(""), address_(""), phone_(""), lon_(0.0), lat_(0.0) {}
+public:
+	Defibrilator():
+		number_(0), name_(""), address_(""), phone_(""), coordinates_() {}
 
-        Defibrilator(int number, std::string name, std::string address, std::string phone, double lon, double lat):
-            number_(number), name_(name), address_(address), phone_(phone), lon_(lon), lat_(lat) {}
+	Defibrilator(int number, std::string name, std::string address, std::string phone, double lon, double lat):
+		number_(number), name_(name), address_(address), phone_(phone), coordinates_(lon, lat) {}
 
-        Defibrilator(const std::string & defibString)
-        {
-            auto strings = splitString(defibString);
-            number_ = std::stoi(strings[0]);
-            name_ = strings[1];
-            address_ = strings[2];
-            phone_ = strings[3];
-            lon_ = parseDouble(strings[4]);
-            lat_ = parseDouble(strings[5]);
-        }
+	Defibrilator(const std::string & defibString)
+	{
+		auto strings = splitString(defibString);
+		number_ = std::stoi(strings[0]);
+		name_ = strings[1];
+		address_ = strings[2];
+		phone_ = strings[3];
+		coordinates_ = Coordinates(strings[4], strings[5]);
+	}
 
-        std::string toString() const
-        {
-            return std::to_string(number_) + ";"
-                + name_ + ";"
-                + address_ + ";"
-                + phone_ + ";"
-                + std::to_string(lon_) + ";"
-                + std::to_string(lat_);
-        }
+	std::string toString() const
+	{
+		return std::to_string(number_) + ";"
+		+ name_ + ";"
+		+ address_ + ";"
+		+ phone_ + ";"
+		+ coordinates_.toString();
+	}
 
-        void debug() const
-        {
-            std::cerr << toString() << std::endl;
-        }
+	void debug() const
+	{
+		std::cerr << toString() << std::endl;
+	}
 
-    public:
-        int number_;
-        std::string name_, address_, phone_;
-        double lon_, lat_;
+public:
+	int number_;
+	std::string name_, address_, phone_;
+	Coordinates coordinates_;
 };
 
 std::vector<Defibrilator> buildDefibrilatorList(int numberOfDefibrilators)
@@ -81,56 +111,53 @@ std::vector<Defibrilator> buildDefibrilatorList(int numberOfDefibrilators)
 	return defibrilatorList;
 }
 
-double computeDistanceBetweenTwoDefibrilators(const Defibrilator & defibrilator1,
-    const Defibrilator & defibrilator2)
+double computeDistanceBetweenTwoDefibrilators(const Coordinates & point1, const Coordinates & point2)
 {
-    double x = (defibrilator2.lon_ - defibrilator1.lon_) * cos((defibrilator1.lat_ + defibrilator2.lat_) / 2);
-    double y = defibrilator2.lat_ - defibrilator1.lat_;
-    static constexpr int kEarthRadius = 6371;
-    return sqrt(x * x + y * y) * kEarthRadius;
+	double x = (point2.lon_ - point1.lon_) * cos((point1.lat_ + point2.lat_) / 2);
+	double y = point2.lat_ - point1.lat_;
+	static constexpr int kEarthRadius = 6371;
+	return sqrt(x * x + y * y) * kEarthRadius;
 }
 
-
-std::size_t getClosestDefribilatorIndex(const std::vector<Defibrilator> & defibrilators, const Defibrilator & inputPoint)
+std::size_t getClosestDefribilatorIndex(const std::vector<Defibrilator> & defibrilators, const Coordinates & inputPoint)
 {
 	std::size_t closestDefibrilatorIndex = SIZE_MAX;
 	double minimumDistanceToInputPoint = std::numeric_limits<double>::infinity();
 	for (size_t index = 0; index < defibrilators.size(); index++)
 	{
-		 const Defibrilator & defibrilator = defibrilators[index];
+		const Defibrilator & defibrilator = defibrilators[index];
 
-		 double distanceToInputPoint = computeDistanceBetweenTwoDefibrilators(inputPoint, defibrilator);
+		double distanceToInputPoint = computeDistanceBetweenTwoDefibrilators(inputPoint, defibrilator.coordinates_);
 
-		 if (distanceToInputPoint < minimumDistanceToInputPoint)
-		 {
-			  closestDefibrilatorIndex = index;
-			  minimumDistanceToInputPoint = distanceToInputPoint;
-		 }
+		if (distanceToInputPoint < minimumDistanceToInputPoint)
+		{
+			closestDefibrilatorIndex = index;
+			minimumDistanceToInputPoint = distanceToInputPoint;
+		}
 	}
 	return closestDefibrilatorIndex;
 }
 
 int main()
 {
-    // Read input parameters
-    std::string longitude;
-    std::cin >> longitude; std::cin.ignore();
+	// Read input parameters to create the inout point
+	std::string longitudeString;
+	std::cin >> longitudeString; std::cin.ignore();
 
-    std::string latitude;
-    std::cin >> latitude; std::cin.ignore();
+	std::string latitudeString;
+	std::cin >> latitudeString; std::cin.ignore();
 
-    int numberOfDefibrilators;
-    std::cin >> numberOfDefibrilators; std::cin.ignore();
+	int numberOfDefibrilators;
+	std::cin >> numberOfDefibrilators; std::cin.ignore();
 
-    // Create a fake defib for input point
-    Defibrilator inputPoint("0;;;;" + longitude + ";" + latitude);
+	Coordinates inputPoint(longitudeString, latitudeString);
 
-    // Build directory of defibs
-    auto defibrilators = buildDefibrilatorList(numberOfDefibrilators);
+	// Build directory of defibs
+	auto defibrilators = buildDefibrilatorList(numberOfDefibrilators);
 
-	 // Look for closest defibrilator
-    std::size_t closestDefibrilatorIndex = getClosestDefribilatorIndex(defibrilators, inputPoint);
+	// Look for closest defibrilator
+	std::size_t closestDefibrilatorIndex = getClosestDefribilatorIndex(defibrilators, inputPoint);
 
-    // Send back answer
-    std::cout << defibrilators[closestDefibrilatorIndex].name_ << std::endl;
+	// Send back answer
+	std::cout << defibrilators[closestDefibrilatorIndex].name_ << std::endl;
 }
